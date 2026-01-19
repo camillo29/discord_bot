1e131ae94c2215201208521fba5570e25528a196
Kamil <kamil.swiatek33@gmail.com>
2026-01-19 16:51:49 +0100

    add shake cooldown

--- app.js
+++ app.js
@@ -50,8 +50,10 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
         if (name === 'szejk') {
             // Send a message containing random shake gif
             if (shakeCooldown.get(user.id) !== null && Math.abs(new Date() - shakeCooldown.get(user.id)) < 24 * 60 * 60 * 1000) {
-                const diff = Math.abs(new Date() - shakeCooldown.get(user.id));
-                console.log('user '+ user.name + ' ma cooldown, jeszcze: ' + Math.floor(diff / (60 * 60 * 1000)) + ':' + Math.floor(diff / (60 * 1000)) + ':' + Math.floor(diff / 1000) + ' diff: ' + diff);
+                const diff = (24 * 60 * 60 * 1000) - new Date() - shakeCooldown.get(user.id)
+                const hours = Math.floor(diff / (1000 * 60 * 60));
+                const minutes = Math.floor((diff / (1000 * 60)) % 60);
+                console.log('user '+ user.name + ' ma cooldown, jeszcze: ' + hours + ':' + minutes + ':' + Math.floor(diff / 1000) + ' diff: ' + diff);
             }
             let content = getRandomShake();
             if (content.includes('20251130_124448')
